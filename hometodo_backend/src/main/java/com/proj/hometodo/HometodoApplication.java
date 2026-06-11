cedeb6d4a15b317aae4bd3e695eaa278515c20b1
Soum <tecsoumyajeet@gmail.com>
2026-06-11 22:34:00 +0530

    first commit

---
+++ hometodo_backend/src/main/java/com/proj/hometodo/HometodoApplication.java
@@ -0,0 +1,13 @@
+package com.proj.hometodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometodoApplication.class, args);
+	}
+
+}
